caf56c0c1f984f5ef7ea6873ffc5f839c9bd0e4f
kresna <kresna.wijaya@indomaret.co.id>
2024-06-09 22:27:51 +0700

    commit

--- api/webhook.js
+++ api/webhook.js
@@ -8,15 +8,10 @@ require("dotenv").config();
 const app = express();
 app.use(bodyParser.json());
 
-// Store OTPs and chat IDs in-memory for simplicity (use a database in production)
-let otpStorage = {};
-let chatIdStorage = {};
-let refStorage = {};
-
 // Function to generate a random reference number
-const generateRef = () => {
-  return randomize("a0", 6);
-};
+// const generateRef = () => {
+//   return randomize("a0", 6);
+// };
 
 // Function to generate OTP
 const generateOTP = () => {
@@ -41,31 +36,13 @@ app.post("/api/webhook", (req, res) => {
     const chatId = message.chat.id;
     const responseMessage = `Hello, you said: ${message.text}`;
 
-    // axios
-    //   .post(`${TELEGRAM_API}/sendMessage`, {
-    //     chat_id: chatId,
-    //     text: responseMessage,
-    //   })
-    //   .then(() => {
-    //     res.status(200).send("Message sent");
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error sending message", error);
-    //     res.status(500).send(message);
-    //     // res.status(500).send('Error sending message');
-    //   });
-
     if (message.text === "otp") {
-      console.log(botToken, chatId, "ini start");
+      console.log(BOT_TOKEN, chatId, "ini start");
 
-      const ref = generateRef();
+      const otp = generateOTP();
       const timestamp = moment().format("YYYY-MM-DD HH:mm:ss");
-      const replyMessage = `Permintaan Kode Verifikasi OTP\n\nNo. Ref: ${ref}\nTgl/Jam: ${timestamp}`;
+      const replyMessage = `Permintaan Kode Verifikasi OTP\n\n HATI HATI PENIPUAN\n\nOTP: ${otp}\nTgl/Jam: ${timestamp}`;
 
-      // Store the ref to be used later
-      refStorage[ref] = chatId;
-
-      // Send the reply message
       axios
         .post(`${TELEGRAM_API}/sendMessage`, {
           chat_id: chatId,
@@ -83,42 +60,19 @@ app.post("/api/webhook", (req, res) => {
           res.status(500).send("Failed to send start message");
         });
     } else {
-      console.log(botToken, chatId, "ini else");
-      const refMatch = text.match(/No\. Ref: (\w+)/);
-      if (refMatch && refMatch[1]) {
-        console.log("masuk sini 1");
-        const ref = refMatch[1];
-        if (refStorage[ref] === chatId) {
-          console.log("masuk sini 2");
-
-          // Generate OTP and store it
-          const otp = generateOTP();
-          otpStorage[chatId] = otp;
-
-          // Send OTP to the user
-          const otpMessage = `Your OTP code is ${otp}`;
-          axios
-            .post(`${TELEGRAM_API}/sendMessage`, {
-              chat_id: chatId,
-              text: otpMessage,
-            })
-            .then((response) => {
-              console.log("OTP sent:", response.data);
-              res.status(200).send("OTP sent successfully");
-            })
-            .catch((error) => {
-              console.error(
-                "Error sending OTP:",
-                error.response ? error.response.data : error.message
-              );
-              res.status(500).send("Failed to send OTP");
-            });
-        } else {
-          res.status(400).send("Invalid reference number or chat ID mismatch");
-        }
-      } else {
-        res.status(400).send("Invalid message format");
-      }
+      axios
+        .post(`${TELEGRAM_API}/sendMessage`, {
+          chat_id: chatId,
+          text: responseMessage,
+        })
+        .then(() => {
+          res.status(200).send("Message sent");
+        })
+        .catch((error) => {
+          console.error("Error sending message", error);
+          res.status(500).send(message);
+          // res.status(500).send('Error sending message');
+        });
     }
   } else {
     res.status(200).send("No message to process");
